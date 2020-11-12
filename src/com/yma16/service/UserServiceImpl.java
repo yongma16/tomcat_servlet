1284af860eb6a124537efae0dc4e07deee3538d8
yongma16 <17685043634@163.com>
2020-11-12 11:28:12 +0800

    注册功能

--- src/com/yma16/service/UserServiceImpl.java
+++ src/com/yma16/service/UserServiceImpl.java
@@ -9,16 +9,37 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService{//实现service接口
     private UserDao userdao=new UserDaoImpl();//user接口功能
+    @Override
+    public User register(String name, String password) {
+        User result=null;
+        System.out.println("loading register");
+        try{
+            Dbutils.begin();
+            User user=userdao.insert(name,password);//insert添加数据
+            System.out.println(user);//数据添加结果
+            if(user!=null){
+                result=user;//数据添加成功则传递
+            }
+            Dbutils.commit();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return result;//返回
+    }
 
     @Override
     public User login(String name, String password) {
         User result=null;
+        System.out.println("loading login");
         try{
             Dbutils.begin();
             User user=userdao.select(name);//名字查询
+            System.out.println(user);//查询的结果
             if(user!=null){
                 if(user.getUserpassword().equals(password))
                 {
+
                     result=user;//查询存在则给result
                 }
             }
