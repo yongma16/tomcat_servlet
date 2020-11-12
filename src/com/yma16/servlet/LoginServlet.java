3e1c69dbd7fc17161cd13ec32040b947bb655110
yongma16 <17685043634@163.com>
2020-11-12 11:31:49 +0800

    添加登陆注册

---
+++ src/com/yma16/servlet/LoginServlet.java
@@ -0,0 +1,62 @@
+package com.yma16.servlet;
+
+import com.yma16.entity.User;
+import com.yma16.service.UserService;
+import com.yma16.service.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        System.out.println("get");
+        req.setCharacterEncoding("UTF-8");//统一UTF-8
+
+        //get参数
+        String username=req.getParameter("username");
+        String password=req.getParameter("password");
+        System.out.println(username+"\t"+password);
+        resp.setContentType("text/html;charaset=UTF-8");
+        //调用登陆业务功能逻辑
+        UserService userService=new UserServiceImpl();//调用接口功能
+        User user=userService.login(username,password);//调用sql查询
+        PrintWriter printWriter=resp.getWriter();
+        if(user!=null){
+            printWriter.println("通过jdbc登陆成功！");
+        }
+        else{
+            printWriter.println("通过jdbc登陆失败");//未定义跳转
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("post");
+        req.setCharacterEncoding("UTF-8");//统一UTF-8
+
+        //get参数
+        String username=req.getParameter("username");
+        String password=req.getParameter("password");
+        System.out.println(username+"\t"+password);
+        resp.setContentType("text/html;charaset=UTF-8");
+        //调用登陆业务功能逻辑
+        UserService userService=new UserServiceImpl();//调用接口功能
+        User user=userService.login(username,password);//调用sql查询
+        PrintWriter printWriter=resp.getWriter();
+        if(user!=null){
+            printWriter.println("通过jdbc登陆成功！");
+        }
+        else{
+            printWriter.println("通过jdbc登陆失败");//未定义跳转
+        }
+    }
+}
