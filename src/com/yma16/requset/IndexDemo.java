9a81c496f7f12486e8153ad89c791eba57936843
yongma16 <17685043634@163.com>
2020-11-12 15:23:09 +0800

    cookie和session测试

--- src/com/yma16/requset/IndexDemo.java
+++ src/com/yma16/requset/IndexDemo.java
@@ -10,14 +10,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 //注册
-@WebServlet("/register")
-public class RegisterServlet extends HttpServlet {
+@WebServlet("/index")
+public class IndexDemo extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //重写get方法，tomcat7以上的版本有个收参的问题
-        String username=req.getParameter("username");//get 用户名
-        String userpassword=req.getParameter("password");//get 密码
-        System.out.println("提交的数据"+username+"\t"+userpassword);
+        Cookie cookie=new Cookie("name","yma16");
+        cookie.setPath("/tomcat_servlet_war_exploded/register");//设置路径
+        cookie.setMaxAge(20);//生命周期（0表示直到浏览器关闭，大于0代表毫秒数，小于0代表浏览器内部存储）
+        resp.addCookie(cookie);//添加cookies传递
+        req.getRequestDispatcher("cookies.html").forward(req,resp);//转发重定向
     }
 
     @Override
@@ -25,19 +27,17 @@ public class RegisterServlet extends HttpServlet {
 
 //        super.doPost(req, resp);
         req.setCharacterEncoding("UTF-8");//统一采用utf-8
-        String username=req.getParameter("username");//get 用户名
-        String userpassword=req.getParameter("password");//get 密码
-        System.out.println("提交的数据"+username+"\t"+userpassword);
 
         resp.setCharacterEncoding("UTF-8");//设置服务端
         resp.setHeader("Content-Type","text/html;character=UTF-8");//设置header
         //同时设置response
         //resp.setContentType("text/html;character=UTF-8");
         PrintWriter printWriter=resp.getWriter();
-
         //创建cookies
-        Cookie cookie=new Cookie("value","yma16");
-        resp.addCookie(cookie);//添加cookies
+        Cookie cookie=new Cookie("name","yma16");
+        cookie.setPath("/tomcat_servlet_war_exploded/register");//设置路径
+        cookie.setMaxAge(20);//生命周期（0表示直到浏览器关闭，大于0代表毫秒数，小于0代表浏览器内部存储）
+        resp.addCookie(cookie);//添加cookies传递
         printWriter.println("注册成功！");
     }
 }
